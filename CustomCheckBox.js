3637db298dc9da55ba30a8da15d14c7ed77da611
Peter-Dobbins <peter_stix_dobbins@yahoo.com>
2023-08-09 10:39:15 +0100

    new file:   CustomCheckBox.js 	modified:   App.js 	modified:   ImageWithShadow.js 	deleted:    assets/artists.png 	modified:   assets/artists1.png 	deleted:    assets/blue.png 	modified:   assets/easy.png 	deleted:    assets/green.png 	deleted:    assets/instructions.png 	modified:   assets/instructions1.png 	deleted:    assets/levels.png 	modified:   assets/levels1.png 	deleted:    assets/pg3img1.jpg 	modified:   assets/tough.png 	modified:   assets/vid.mp4 	deleted:    assets/yellow.png 	modified:   package-lock.json 	modified:   package.json

---
+++ CustomCheckBox.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const CustomCheckBox = ({ label, isChecked, onValueChange }) => {
+  return (
+    <TouchableOpacity 
+      style={styles.container} 
+      onPress={() => onValueChange(!isChecked)}
+    >
+      <Text style={[styles.box, isChecked && styles.checked]}>{isChecked ? '✓' : ''}</Text>
+      {label && <Text style={styles.label}>{label}</Text>}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  box: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginRight: 5,
+    textAlign: 'center',
+    lineHeight: 18,
+  },
+  checked: {
+    backgroundColor: '#eee',
+  },
+  label: {
+    fontSize: 16,
+  },
+});
+
+export default CustomCheckBox;
